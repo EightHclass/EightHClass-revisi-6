ce9659de95e87f6c26750382aa6b5a855fa3ae71
EightHclass <142408807+EightHclass@users.noreply.github.com>
2023-09-01 17:22:13 +0700

    V3! (web revisi 6)

---
+++ redir.js
@@ -0,0 +1,7 @@
+function redirInstagram() {
+    window.location.href = "https://instagram.com/wiseexplorers_8h?igshid=MzRlODBiNWFlZA=="
+}
+
+function redirNgl() {
+    window.location.href = "https://ngl.link/wiseexplorers_8h"
+}
